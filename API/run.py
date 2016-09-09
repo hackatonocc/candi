a4eed81610777463c412852bd2b292871c5b4e15
fleija117 <fleija@occ.com.mx>
2016-09-09 11:36:30 -0500

    POST
    Creation de usuarios

--- API/run.py
+++ API/run.py
@@ -1,67 +1,61 @@
 #author Rolando Leija
 from flask import Flask, request
 import json
+from flask import jsonify
 from bson import json_util
 from bson.objectid import ObjectId
 from pymongo import MongoClient
+import random
 
 # Flask
 app = Flask(__name__)
-
-# MongoDB connection
-#MONGO_HOST = 'http://ec2-54-245-43-185.us-west-2.compute.amazonaws.com/'
-#var comments = db.Matches.find({User_id: "jopuca"},{"JobOffer_id": true})
-#var OfersIds = comments.map(function(c) { return c.JobOffer_id; });
-#db.JobOffers.find({"_id": {$nin: OfersIds}});
-
-
-
 connection = MongoClient('ec2-54-245-43-185.us-west-2.compute.amazonaws.com', 27017)
 db = connection.CANDI
 def toJson(data):
 	"""Convert Mongo object(s) to JSON"""
 	return json.dumps(data, default=json_util.default)
 
+def genUserID(data):
+    data = data.lower().replace(" ", "")
+    data = data[0:7]
+    return data + str(random.randrange(100, 1001, 2))
+
 @app.route('/JobOffers', methods=['GET'])
 def JobOffers():
-
   if request.method == 'GET':
-    #lim = int(request.args.get('limit', 10))
-    #off = int(request.args.get('offset', 0))
-    #comments = db['Matches'].find({"User_id": "jopuca"},{"JobOffer_id": "true"})
     results = db['JobOffers'].find()
     json_results = []
-    #match = []
-    #joboffer = []
-    #merged = {}
-    # get a list of ids and author_ids for every message
-    #for iden in db['Matches'].find({"User_id": "jopuca"},{"JobOffer_id": "true"}):
-        #match.append(iden['_id'])
-    # iterate through every author_ids to find the corresponding username
-    #for _id, job in joboffer:
-     #   db['JobOffers'].find({""},{"_id":true})
-
-    #    author = coll_user.find_one({"_id": item}, {"username": 1})
-    #    merged = dict(chain((match.items() + joboffer.items())))
-
     for result in results:
         json_results.append(result)
     return toJson(result)
 
-#@app.route('/sightings/<sighting_id>', methods=['GET'])
-#def sighting(sighting_id):
-#  """Return specific UFO sighting
-#  ex) GET /sightings/123456
-# """
-#  if request.method == 'GET':
-#    result = db['ufo'].find_one({'_id': ObjectId(sighting_id)})
-#    return toJson(result)
+@app.route('/registro/', methods=['POST'])
+def insert():
+    user = db['Users']
+    idfirst = request.json['id']
+    password = request.json['password']
+    name = request.json['name']
+    mail = request.json['mail']
+    profesion = request.json['profesion']
+    #skills = request.json['skills']
+    #phone = request.json['phone']
+    iduser = genUserID(name)
+
+    user.insert({'_id': iduser, 'Password': password, 'CreationDate' : 'Wed Fri 09 2016 13:36:48 GMT-0500', 'Active' : 'true', 'EmailAddress' : mail, 'Name': name, 'JobSeeker' : {'Profession': profesion }})
+    #,'JobSeeker' : {'Profession': profesion, 'Skills': [skills], 'Phone' : phone } })
+    results = user.find_one({"User_id": '"' + str(iduser) + '"' })
+
+    json_results = []
+    for result in results:
+        json_results.append(result)
+    return toJson(result)
 
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  response.headers.add('Candier', 'by OCCMundial')
   return response
 
 if __name__ == '__main__':
